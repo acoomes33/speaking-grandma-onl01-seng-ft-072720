450d77d7feaedcd95585d2a60fa17c1c83d2a0d9
Alan Coomes <acoomes1@cord.edu>
2020-07-27 23:14:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,8 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase.upcase = false
-    puts "HUH?! Speak UP, SONNY!"
-  elsif 
+  if phrase == phrase.upcase
+    puts "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
 end
 
